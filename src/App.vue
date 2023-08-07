0d2f7d4b0622dd870be301021873b7ba1576e676
tamaina <tamaina@hotmail.co.jp>
2023-08-07 07:09:08 +0000

    :v:

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <input type="file" ref="input" @change="onInputChange">
     <video :class="$style.video" :src="sourceUrl" controls />
     <video :class="$style.video" :src="video" controls />
-    <br />
     <button @click="transcode">Start</button>
+    <button @click="logs = []">Purge Logs</button>
     <div :class="$style.logs">
       <p v-for="log in logs" v-text="log" />
     </div>
@@ -15,12 +15,12 @@
 // https://github.com/ffmpegwasm/ffmpeg.wasm/blob/a6eb8af90471808fcf79afd4e232f68f750ea218/apps/vue-vite-app/src/components/FFmpegDemo.vue
 import { FFmpeg } from '@ffmpeg/ffmpeg'
 import type { LogEvent } from '../node_modules/@ffmpeg/ffmpeg/dist/esm/types'
-import coreURL from '../node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.js?url'
-import workerURL from '../node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.worker.js?url'
-import wasmURL from '../node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.wasm?url'
+import coreURL from '../node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.js?url'
+import wasmURL from '../node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.wasm?url'
 import { computed, ref } from 'vue'
 
-const ffmpeg = new FFmpeg()
+const VOUTDIR = './out'
+
 const video = ref('')
 const source = ref(null as File | null)
 const input = ref<HTMLInputElement>();
@@ -32,25 +32,19 @@ function onInputChange() {
 }
 
 async function transcode() {
+  const ffmpeg = new FFmpeg()
   logs.value.push('Loading ffmpeg-core.js')
   ffmpeg.on('log', ({ message: msg }: LogEvent) => {
     logs.value.push(msg)
   })
-  await ffmpeg.load({ coreURL, workerURL, wasmURL })
+  await ffmpeg.load({ coreURL, wasmURL })
   if (!source.value) return;
   logs.value.push('Start transcoding')
-  const fr = await (() => {
-    const fr = new FileReader()
-    return new Promise<FileReader>((resolve) => {
-      fr.onload = () => resolve(fr);
-      fr.readAsArrayBuffer(source.value as File)
-    });
-  })();
-  await ffmpeg.writeFile('test.avi', new Uint8Array(fr.result as ArrayBuffer))
-  await ffmpeg.exec(['-i', 'test.avi', '-movflags', '+faststart', '-fflags', '+bitexact', 'test.mp4'])
-  logs.value.push('Complete transcoding')
-  const data = await ffmpeg.readFile('test.mp4')
-  video.value = URL.createObjectURL(new Blob([(data as Uint8Array).buffer], { type: 'video/mp4' }))
+  await ffmpeg.createDir(VOUTDIR);
+  await ffmpeg.writeFile('test.avi', new Uint8Array(await source.value.arrayBuffer()))
+  const segCode = await ffmpeg.exec(['-i', 'test.avi', '-map', '0', '-c', 'copy', '-flags', '+global_header', '-f', 'segment', '-segment_time', '2', '-segment_format_options', 'movflags=+faststart', '-reset_timestamps', '1', `${VOUTDIR}/%10d.mp4`])
+  logs.value.push(`Complete transcoding @${segCode}`)
+  logs.value.push((await ffmpeg.listDir(VOUTDIR)).filter(x => !x.isDir).map(x => x.name).join('\n'));
 }
 </script>
 
@@ -69,11 +63,16 @@ async function transcode() {
   display: flex;
   flex-direction: column;
   max-width: 80vw;
+
+  > * {
+    margin: 8px;
+  }
 }
 
 .logs {
   text-align: left;
   font-family: 'Courier New', Courier, monospace;
+  white-space: pre-wrap;
 }
 
 .video {
